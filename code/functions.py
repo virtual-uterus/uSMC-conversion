d10e5d8126d31b277b6579af167b927cdfa1309b
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-18 12:48:30 +1300

    New values of stimulus that produce better results

--- code/functions.py
+++ code/functions.py
@@ -13,10 +13,10 @@ import scipy.stats as stat
 
 # Specific values for different estrus stages
 ESTRUS = {
-    "estrus": {"gkv43": 2.212, "stim_current": -0.5, "P4": 14, "E2": 45},
-    "proestrus": {"gkv43": 1.2, "stim_current": -0.5, "P4": 30, "E2": 80},
-    "metestrus": {"gkv43": 2.3, "stim_current": -0.25, "P4": 25, "E2": 43},
-    "diestrus": {"gkv43": 1.4, "stim_current": -0.5, "P4": 15, "E2": 70}
+    "estrus": {"gkv43": 2.5, "stim_current": -0.4, "P4": 14, "E2": 40},
+    "proestrus": {"gkv43": 0.95, "stim_current": -0.4, "P4": 37, "E2": 100},
+    "metestrus": {"gkv43": 2.04, "stim_current": -0.35, "P4": 25, "E2": 49},
+    "diestrus": {"gkv43": 1.155, "stim_current": -0.2, "P4": 10, "E2": 90},
 }
 
 # Hard coded values of the P4 dependent constants
