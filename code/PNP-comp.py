368e3b6092961180ae061e296de2e518a66e616a
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-18 11:04:02 +1300

    Formatting fixes

--- code/PNP-comp.py
+++ code/PNP-comp.py
@@ -16,14 +16,27 @@ import plots
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Compares the pregnant and non-pregnant models")
+        description="Compares the pregnant and non-pregnant models"
+    )
 
-    parser.add_argument("metric", type=str, choices={"l2", "rmse", "mae"},
-                        help="comparison metric")
-    parser.add_argument("-p", "--plot-only", action="store_true",
-                        help="flag used just to plot data")
-    parser.add_argument("-m", "--metric-only", action="store_true",
-                        help="flag used just to compute metric")
+    parser.add_argument(
+        "metric",
+        type=str,
+        choices={"l2", "rmse", "mae"},
+        help="comparison metric",
+    )
+    parser.add_argument(
+        "-p",
+        "--plot-only",
+        action="store_true",
+        help="flag used just to plot data",
+    )
+    parser.add_argument(
+        "-m",
+        "--metric-only",
+        action="store_true",
+        help="flag used just to compute metric",
+    )
 
     args = parser.parse_args()
     init_states_M, constants_M = Means2023.initConsts()
@@ -45,33 +58,38 @@ if __name__ == "__main__":
 
         for i, key in enumerate(functions.ESTRUS.keys()):
             # Set estrus dependant constants
-            constants_R = functions.setEstrusParams(constants_R,
-                                                    legend_constants_R, key)
+            constants_R = functions.setEstrusParams(
+                constants_R, legend_constants_R, key
+            )
 
             print("Computing Roesler2024 {} simulation".format(key))
 
-            voi_R, states_R, _ = Roesler2024.solveModel(init_states_R,
-                                                        constants_R)
+            (
+                voi_R,
+                states_R,
+                _,
+            ) = Roesler2024.solveModel(init_states_R, constants_R)
 
             sim_output[key] = states_R[0, :]  # Membrane potential for plot
             comp_points[i] = functions.computeComparison(
-                states_M, states_R, args.metric)
+                states_M, states_R, args.metric
+            )
 
             # Reset the model
             init_states_R, constants_R = Roesler2024.initConsts()
 
         sim_output["time"] = voi_R / 1000  # Add timesteps in s
 
-        with open(comp_file, 'wb') as handler:
+        with open(comp_file, "wb") as handler:
             # Pickle data
             pickle.dump(comp_points, handler)
 
-        with open(sim_file, 'wb') as handler:
+        with open(sim_file, "wb") as handler:
             # Pickle data
             pickle.dump(sim_output, handler)
 
     else:
-        with open(sim_file, 'rb') as handler:
+        with open(sim_file, "rb") as handler:
             # Unpickle data
             sim_output = pickle.load(handler)
 
@@ -79,9 +97,10 @@ if __name__ == "__main__":
             # Compute just the metric
             for i, key in enumerate(functions.ESTRUS.keys()):
                 comp_points[i] = functions.computeComparison(
-                    sim_output["means"], sim_output[key], args.metric)
+                    sim_output["means"], sim_output[key], args.metric
+                )
 
-            with open(comp_file, 'wb') as handler:
+            with open(comp_file, "wb") as handler:
                 # Pickle data
                 pickle.dump(comp_points, handler)
 
