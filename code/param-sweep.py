55d28ca6e6bf029648079bfc0d5871cc38db9e82
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-18 10:28:20 +1300

    Fixed some issues with lines being too long

--- code/param-sweep.py
+++ code/param-sweep.py
@@ -15,35 +15,56 @@ import plots
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Perform a parameter sweep for a given parameter")
+        description="Perform a parameter sweep for a given parameter"
+    )
 
     parser.add_argument("param", type=str, help="name of the parameter")
-    parser.add_argument("metric", type=str, choices={"l2", "rmse", "mae"},
-                        help="comparison metric")
+    parser.add_argument(
+        "metric",
+        type=str,
+        choices={"l2", "rmse", "mae"},
+        help="comparison metric",
+    )
 
     # Create subparser for the sweep and plot commands
     subparser = parser.add_subparsers()
 
     # Sweep subcommand arguments
-    sweep_subparser = subparser.add_parser("sweep",
-                                           help="performs parameter sweep")
-    sweep_subparser.add_argument("start_val", type=float,
-                                 metavar="start-value",
-                                 help="value to start the sweep at")
-    sweep_subparser.add_argument("end_val", type=float, metavar="end-value",
-                                 help="value to end the sweep at")
-    sweep_subparser.add_argument("nb_points", metavar="nb-points", type=int,
-                                 help="number of points for the"
-                                 "parameter sweep")
-    sweep_subparser.add_argument("--estrus", type=str,  default="all",
-                                 choices={"estrus", "metestrus",
-                                          "proestrus", "diestrus", "all"},
-                                 help="estrus stage")
+    sweep_subparser = subparser.add_parser(
+        "sweep",
+        help="performs parameter sweep",
+    )
+    sweep_subparser.add_argument(
+        "start_val",
+        type=float,
+        metavar="start-value",
+        help="value to start the sweep at",
+    )
+    sweep_subparser.add_argument(
+        "end_val",
+        type=float,
+        metavar="end-value",
+        help="value to end the sweep at",
+    )
+    sweep_subparser.add_argument(
+        "nb_points",
+        metavar="nb-points",
+        type=int,
+        help="number of points for the parameter sweep",
+    )
+    sweep_subparser.add_argument(
+        "--estrus",
+        type=str,
+        default="all",
+        choices={"estrus", "metestrus", "proestrus", "diestrus", "all"},
+        help="estrus stage",
+    )
 
     # Plot subcommand arguments
-    plot_subparser = subparser.add_parser("plot",
-                                          help="plots results without"
-                                          "doing a parameter sweep")
+    plot_subparser = subparser.add_parser(
+        "plot",
+        help="plots results without doing a parameter sweep",
+    )
 
     # Parse input arguments
     args = parser.parse_args()
@@ -52,7 +73,7 @@ if __name__ == "__main__":
 
     # Error check
     try:
-        assert (args.start_val < args.end_val)
+        assert args.start_val < args.end_val
 
     except AssertionError:
         sys.stderr.write("Error: start value must be smaller than end value\n")
@@ -68,22 +89,30 @@ if __name__ == "__main__":
 
         # Error check and index retrival
         try:
-            _, idx = functions.setParams(constants, legend_constants,
-                                         args.param, None)
+            breakpoint()
+            _, idx = functions.setParams(
+                constants,
+                legend_constants,
+                args.param,
+                None,
+            )
 
         except IndexError:
             sys.stderr.write(
                 "Error: parameter sweep for {} can't be done\n".format(
-                    args.param))
+                    args.param,
+                )
+            )
             exit(1)
 
         # Error check
         try:
-            assert (args.nb_points > 0)
+            assert args.nb_points > 0
 
         except AssertionError:
             sys.stderr.write(
-                "Error: the number of points must be greater than 0\n")
+                "Error: the number of points must be greater than 0\n",
+            )
             exit(1)
 
         if args.estrus == "all":
@@ -94,8 +123,11 @@ if __name__ == "__main__":
             estrus_stage = [args.estrus]
 
         for estrus in estrus_stage:
-            constants = functions.setEstrusParams(constants, legend_constants,
-                                                  estrus)
+            constants = functions.setEstrusParams(
+                constants,
+                legend_constants,
+                estrus,
+            )
 
             print("{} stage".format(estrus.capitalize()))
 
@@ -113,13 +145,15 @@ if __name__ == "__main__":
                 constants[idx] = value
                 _, states, _ = Roesler2024.solveModel(init_states, constants)
                 comp_points[i] = functions.computeComparison(
-                    orig_states, states, args.metric)
+                    orig_states, states, args.metric
+                )
 
             print("  Writing results\n")
             output_file = "../res/{}_{}_{}_sweep.pkl".format(
-                args.param, estrus, args.metric)
+                args.param, estrus, args.metric
+            )
 
-            with open(output_file, 'wb') as handler:
+            with open(output_file, "wb") as handler:
                 pickle.dump([comp_points, values], handler)
 
             # Reset constants
