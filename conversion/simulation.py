ba62e22f34f34aeb60caa345d67335bfb22321aa
Mathias <mroe734@aucklanduni.ac.nz>
2024-11-29 21:47:45 +1300

    Wrote functions for each sub command in param-sweep script

--- conversion/simulation.py
+++ conversion/simulation.py
@@ -10,7 +10,6 @@ Date: 11/24
 
 import os
 
-from conversion import Tong2011, Tong2014, Means2023, Roesler2024, utils
 import numpy as np
 
 from conversion import Tong2011, Tong2014, Means2023, Roesler2024
@@ -218,6 +217,169 @@ def run_sweep(sweep_model, param, values, metric, base_sim, estrus=""):
 
     return comp_points
 
+
+def sweep_func(args):
+    """Function called by the param-sweep script to run the parameter sweep
+
+    The data for the comparison is saved in RES_DIR.
+
+    Args:
+    args -- argparse.Namespace with following arguments:
+      base_model -- str, name of the base model for comparison from
+      {"Roesler2024", "Means2023", "Tong2011", "Tong2014"}.
+      sweep_model -- str, name of the model to use from
+      {"Roesler2024", "Means2023", "Tong2011", "Tong2014"}.
+      metric -- str, name of the metric to use from
+      {l2, rmse, mae, correl}.
+      param -- str, name of the parameter to sweep over.
+      start_val -- float, value to start the sweep at.
+      end_val -- float, value to end the sweep at.
+      nb_points -- int, number of points for the parameter sweep
+      estrus -- str, estrus stage for the Roesler2024 model, default value "".
+
+    Returns:
+    plot_data -- list(tuple), list of comparison points, parameter values,
+    and the estrus stage for each sweep.
+
+    Raises:
+    ValueError -- if the start number is less than 0.
+    ValueError -- if the end number is smaller than start value.
+    ValueError -- if nb_steps is not an integer.
+    ValueError -- if the model name is incorrect.
+    ValueError -- if the provided metric is not one of
+    {'l2', 'rmse', 'mae', 'correl'}.
+    ValueError -- if the start value is smaller than the end value.
+    ValueError -- if the number of simulations is negative.
+    ValueError -- if the number of simulations is not an integer.
+    KeyError -- if the estrus stage is incorrect.
+
+
+    """
+    try:
+        # Check that sweep parameters are valid
+        check_sweep_parameters(
+            args.start_val,
+            args.end_val,
+            args.nb_points,
+        )
+
+        # Compute base model if pregnant model
+        if args.base_model != "Roesler2024":
+            print(f"Computing {args.base_model} simulation with default times")
+            t, base_data = run_simulation(args.base_model)
+
+        # Create values to loop through
+        values = np.linspace(args.start_val, args.end_val, args.nb_points)
+
+        # Create arrays to store results
+        comp_points = np.zeros(len(values))
+        plot_data = []
+
+        if args.estrus == "all":
+            estrus = ESTRUS
+        else:
+            estrus = [args.estrus]
+
+        for stage in estrus:
+            # Loop over estrus cycle
+            if stage != "":
+                print(f"{stage.capitalize()} stage")
+
+            save_file = utils.sweep_path(
+                args.base_model,
+                args.sweep_model,
+                args.param,
+                args.metric,
+                stage,
+            )
+
+            if args.base_model == "Roesler2024":
+                print(
+                    "Computing {} simulation with default times".format(
+                        args.base_model,
+                    )
+                )
+                t, base_data = run_simulation(
+                    args.base_model,
+                    estrus=stage,
+                )
+
+            # Main sweep
+            comp_points = run_sweep(
+                args.sweep_model,
+                args.param,
+                values,
+                args.metric,
+                base_data[0, :],
+                stage,
+            )
+
+            # Save data and prepare for plotting
+            plot_data.append((comp_points, values, stage))
+            utils.save_data(save_file, (comp_points, values, stage))
+
+    except (ValueError, KeyError):
+        raise
+
+    return plot_data
+
+
+def plot_func(args):
+    """Function called by the param-sweep script to plot the parameter sweep
+
+    The data is loaded from RES_DIR
+
+    Args:
+    args -- argparse.Namespace with following arguments:
+      base_model -- str, name of the base model for comparison from
+      {"Roesler2024", "Means2023", "Tong2011", "Tong2014"}.
+      sweep_model -- str, name of the model to use from
+      {"Roesler2024", "Means2023", "Tong2011", "Tong2014"}.
+      metric -- str, name of the metric to use from
+      {l2, rmse, mae, correl}.
+      param -- str, name of the parameter to sweep over.
+      estrus -- str, estrus stage for the Roesler2024 model, default value "".
+
+    Returns:
+    plot_data -- list(tuple), list of comparison points, parameter values,
+    and the estrus stage for each sweep.
+
+    Raises:
+    FileNotFoundError -- if the results files are not found.
+    ValueError -- if the model name is incorrect.
+    ValueError -- if the provided metric is not one of
+    {'l2', 'rmse', 'mae', 'correl'}.
+    KeyError -- if the estrus stage is incorrect.
+
+
+    """
+    try:
+        plot_data = []
+
+        if args.estrus == "all":
+            estrus = ESTRUS
+        else:
+            estrus = [args.estrus]
+
+        for stage in estrus:
+            # Loop over estrus cycle
+            save_file = utils.sweep_path(
+                args.base_model,
+                args.sweep_model,
+                args.param,
+                args.metric,
+                stage,
+            )
+
+            loaded_data = utils.load_data(save_file)
+            plot_data.append(loaded_data)
+
+    except (FileNotFoundError, ValueError, KeyError):
+        raise
+
+    return plot_data
+
+
 def save_simulation(model_name, sim_data, t, estrus=""):
     """Saves the results of a simulation as {model_name}_{duration}s.pkl with
     duration the last timestep in t. If the model is Roesler2024 then the save
@@ -264,6 +426,9 @@ def check_sweep_parameters(start_val, end_val, nb_points):
     Returns:
 
     Raises:
+    ValueError -- if the start value is smaller than the end value.
+    ValueError -- if the number of simulations is negative.
+    ValueError -- if the number of simulations is not an integer.
     ValueError -- if the start value is smaller than the end value.
     ValueError -- if the number of simulations is negative.
     ValueError -- if the number of simulations is not an integer.
