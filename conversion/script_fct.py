02d209b14a8ef40f5176d2cef14df48f97fc60b2
Mathias <mroe734@aucklanduni.ac.nz>
2025-03-31 12:29:14 +1300

    Removing gkca as well to avoid bug

--- conversion/script_fct.py
+++ conversion/script_fct.py
@@ -172,6 +172,7 @@ def plot_func(args):
 
         if args.param == "all":
             params = PARAM
+            params.pop("gkca")  # Remove stimulus current
             params.pop("stim_current")  # Remove stimulus current
         else:
             params = [args.param]
